4504deb99bf04ad0ab0bba829b76003a1fe68289
Hanna Tuomi <hanna.tuomi@gmail.com>
2018-12-13 14:19:36 -0500

    Update Dot Map_Greeness.py

--- Dot Map_Greeness.py
+++ Dot Map_Greeness.py
@@ -36,7 +36,6 @@ green_data_for_tooltip = {'percent': {}}
 for element in green_data.keys():
     green_data_for_tooltip['percent'][element] = green_data[element][1]
     
-print(green_data_for_tooltip)
     
 #read the csv for zipcodes to make it usable in geoplotlib
 zips = {}
@@ -52,6 +51,12 @@ for line in zipcodes:
     newline[0] = ZIP
     zips[newline[0]] = [newline[0], float((newline[1])), float((newline[2]))]
 
+##create the pop-up box with green change %s
+#manager = HotspotManager()
+#for element in zips.keys():
+#    manager.add_poly(zipselement[1], element[2], )
+#manager.pick()
+#        
 
 #create a new dot for each zipcode
 for element in green_data.keys():
@@ -64,19 +69,16 @@ for element in green_data.keys():
         
         #create the data to make a dot for
         data = {'zip':zips[element][0], 'lat': [(zips[element][1])], 'lon': [(zips[element][2])]}
-       
-        print(green_data_for_tooltip['percent'][element])
         
         #add the dot for the zipcode
         geoplotlib.add_layer(dd(data, [color,255,color],size, \
-            f_tooltip=lambda r: r['percent']))
+            f_tooltip=None))
 #        dd.draw(proj, mouse_x, mouse_y, ui_manager)
         
         #add labels for zipcode
         geoplotlib.labels(data, 'zip' , color='k', font_name= 'helvetica', \
            font_size=8, anchor_x='left', anchor_y='top')
 
-        #create the pop-up box with green change %s
         
 
 geoplotlib.show()
